51080b11aaa045c058bb6af2428a0d64fa5c49db
Jordan Rinke <me@jordanrinke.com>
2017-09-12 16:09:04 -0500

    case fix

--- ui/assets/src/app/claim/claim.component.ts
+++ ui/assets/src/app/claim/claim.component.ts
@@ -58,7 +58,7 @@ export class ClaimComponent implements OnInit {
   constructor(private http: Http, angulartics2GoogleTagManager: Angulartics2GoogleTagManager) { }
 
   useInvite(user){
-    this.http.get('https://alpha.supergiant.io/claim?invite=' + user.invite + '&email=' + user.email).map(
+    this.http.get('https://alpha.supergiant.io/claim?invite=' + user.invite.toUppercase() + '&email=' + user.email).map(
       response => response.json()).subscribe(
       (result) => {
         this.state.status = 1
